d8247b4ccc3b4d85b5250d5383c90340ce173d2d
Hong Hai <honghaiit257@gmail.com>
2026-05-07 12:53:52 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            while (l < r && !isAlphaNum(s.charAt(l))) {
+                l++;
+            }
+            while (l < r && !isAlphaNum(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    public boolean isAlphaNum(char c) {
+        return Character.isDigit(c) || Character.isLetter(c);
+    }
+}
