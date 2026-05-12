2450608ae5706b644bbaa083d4c6c62440f5c96a
Hong Hai <honghaiit257@gmail.com>
2026-05-11 22:47:05 -0700

    Add: valid-word-abbreviation - submission-9

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-9.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0;
+        int j = 0;
+        while (i <= word.length() - 1 && j <= abbr.length() - 1) {
+            if (Character.isLetter(abbr.charAt(j))) {
+                if (word.charAt(i) != abbr.charAt(j)) return false;
+                i++;
+                j++;
+            }else {
+                String length = "";
+                if (abbr.charAt(j) == '0') return false;
+                while (j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
+                    char c = abbr.charAt(j);
+                    length += String.valueOf(c);
+                    j++;
+                }
+                int lengthValue = Integer.parseInt(length);
+                i = i + lengthValue;
+            }
+        }
+        return i == word.length() && j == abbr.length();
+    }
+}
